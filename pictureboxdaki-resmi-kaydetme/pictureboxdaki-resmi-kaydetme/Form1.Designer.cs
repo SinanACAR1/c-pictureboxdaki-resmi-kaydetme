46e191df734eb8f1142f5953ede5b6965e9ea39e
SinanACAR1 <sinanacar14654@gmail.com>
2017-07-17 18:48:44 +0300

    Add files via upload
    İstediğiniz resmi seçip klasöre kayıt etme programı.

---
+++ pictureboxdaki-resmi-kaydetme/pictureboxdaki-resmi-kaydetme/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace pictureboxdaki_resmi_kaydetme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::pictureboxdaki_resmi_kaydetme.Properties.Resources._Teoman_Dede__DWO_T___349_;
+            this.pictureBox1.Location = new System.Drawing.Point(52, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 205);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 35);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Dosya Seç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 337);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Resim Kaydet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ pictureboxdaki-resmi-kaydetme/pictureboxdaki-resmi-kaydetme/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pictureboxdaki_resmi_kaydetme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public enum harfler   //RESİMLERİN İSİMLERİ ÇAKISMAMASI İÇİN RASTGELE HARFLER
+        {
+            a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, r, s, t, u, v, y, z
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int harf = rnd.Next(0, 24);//RESİM ADLARI ÇAKIŞMAMASI İÇİN RASTGELE HARFLER ÜRETİLMESİ
+            int sayi = rnd.Next(0, 100);//RESİM ADLARI ÇAKIŞMAMASI İÇİN RASTGELE SAYI ÜRETİLMESİ
+
+            pictureBox1.Image.Save(Application.StartupPath + "\\aa\\Resim" + ((harfler)(harf)).ToString().ToUpper() + sayi.ToString() + ".png");
+            //YUKARDAKİ "\\aa\\Resim" YAZAN YER RESİMİ NEREYE VE HANGİ ADLA KAYDEDİCEĞİNİ BELİRLER SONDAKİ .PNG YAZISI HANGİ UZANTIDA KAYDEDİCEĞİNİ BELİRLER
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   
+            OpenFileDialog dosya = new OpenFileDialog(); 
+            dosya.Filter = "Resim Dosyası |*.jpg;*.nef;*.png| Video|*.avi| Tüm Dosyalar |*.*";  //dosya uzantılarını filtreliyoruz
+            dosya.Title = "Dosya Seç"; //Başlık
+            dosya.ShowDialog();
+            string DosyaYolu = dosya.FileName;
+            pictureBox1.ImageLocation = DosyaYolu;
+        }
+    }
+}
